4898b29ba57813b073ffbd4c8df17e289b82f3f5
R-Rajkumar <rraju1990@gmail.com>
2014-10-04 10:44:42 +0530

    ProtobufConstants added

---
+++ components/protobuf-services/protobuf-registry/src/main/java/org/wso2/carbon/protobuf/constants/ProtobufConstants.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * 
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.protobuf.constants;
+
+public class ProtobufConstants {
+    public static final String PROTOBUF_SERVER_XML = "protobuf-server.xml";
+    public static final String PROTOBUF_SERVER_XSD = "protobuf-server.xsd";
+    public static final String KEYSTORE_PATH = "Security.KeyStore.Location";
+    public static final String KEYSTORE_PASSWORD = "Security.KeyStore.Password";
+    public static final String TRUSTSTORE_PATH = "Security.TrustStore.Location";
+    public static final String TRUSTSTORE_PASSWORD = "Security.TrustStore.Password";
+}
